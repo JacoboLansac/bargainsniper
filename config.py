bf4753d10d75060f74f6e58cd75dd0f547576892
jacopod <jacolansac@gmail.com>
2022-07-08 08:10:27 +0200

    multiplatform paths

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from os import path
 from logging.config import fileConfig
 import yaml
@@ -8,8 +9,11 @@ logger = logging.getLogger()
 # logger.debug('often makes a very good meal of %s', 'visiting tourists')
 
 # setting up host machine paths
-filesdb_rootpath = "/home/jl/data/filebase"
-project_path = "/home/jl/projects/bargainsniper/"
+filesdb_rootpath = path.join(path.expanduser('~'), 'data', 'filebase')
+project_path = path.dirname(__file__)
+
+os.makedirs(filesdb_rootpath, exist_ok=True)
+
 
 # Configured collections
 with open(path.join(project_path, "collections.yml"), 'r') as stream:
